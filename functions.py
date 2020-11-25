2ff45206d1bb4d112c8cd51eb445f5f554e07cc0
Laurent Fabre <lf@macbook-pro-de-laurent.home>
2020-11-25 04:21:42 +0100

    remove PHPSESSID

--- functions.py
+++ functions.py
@@ -83,7 +83,7 @@ def stick_login(email, password):
             'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15', 
             'Content-Type: application/x-www-form-urlencoded',
             'X-Requested-With: XMLHttpRequest',
-            'Set-Cookie: ' + SESSION
+            #'Set-Cookie: ' + SESSION
         ])
         c.perform()
         return c.getinfo(pycurl.HTTP_CODE)
