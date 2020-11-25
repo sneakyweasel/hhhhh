a83c335c12332d2cf18701ac3cfffb80371f9b0c
Laurent Fabre <lf@macbook-pro-de-laurent.home>
2020-11-25 04:11:51 +0100

    urllib.parse.quote_from_bytes

--- stickitup.py
+++ stickitup.py
@@ -1,11 +1,16 @@
 # hello
 import hashpumpy
+import urllib.parse
 
 def main():
     # admi / '4dd28ad0560441245de6fda3973890eeb41b702d' / variable key length
+    results = []
     for i in range(6, 30):
         data = hashpumpy.hashpump('4dd28ad0560441245de6fda3973890eeb41b702d', "admi", 'n', i)
-        print("{}: {}".format(i, data))
+        results.append(data)
+
+    for r in results:
+        print("{} => {}".format(r[0], urllib.parse.quote_from_bytes(r[1])))
 
 if __name__ == "__main__":
     main()
